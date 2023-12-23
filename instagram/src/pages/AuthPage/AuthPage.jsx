0d07060077856f93c8e92960ecbdc27bf470cc54
Tiago de Oliveira <tiago.zdo@gmail.com>
2023-12-23 01:47:30 -0300

    Configuration Design Auth Page

---
+++ instagram/src/pages/AuthPage/AuthPage.jsx
@@ -0,0 +1,10 @@
+
+
+export const AuthPage = () => {
+    return (
+        <div>AuthPage
+            <p>21:18</p>
+            <p>https://www.youtube.com/watch?v=RMScMwY2B6Q&t=1761s</p>
+        </div>
+    )
+}

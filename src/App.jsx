0e85781bb6f7898579ca5363cc4d1cb46093d55f
Tiago de Oliveira <tiago.zdo@gmail.com>
2023-12-22 21:14:02 -0300

    Data fetching

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
+import { useState, useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap";
 import { Filters } from "./components/Filters/Filters";
 import { Cards } from "./components/Cards/Cards";
 
 export const App = () => {
+    let [pageNumber, setPageNumber] = useState(1);
+    let [fetchedData, updateFetchedData] = useState([]);
+    let { info, results } = fetchedData;
+
+    let api = `https://rickandmortyapi.com/api/character/?page=${pageNumber}`;
+
+    useEffect(() => {
+        (async function () {
+            let data = await fetch(api).then(res => res.json());
+            updateFetchedData(data);
+        })()
+    }, [api]);
+
     return (
         <div>
             <h1 className="text-center ubuntu my-4">

5e1d10f33dd301df3197da426bca74e74a4fe2d1
tiago1820 <tiago.zdo@gmail.com>
2023-12-25 23:32:08 -0300

    Pagination Basics - Rick and Morty

--- rick-and-morty/src/App.jsx
+++ rick-and-morty/src/App.jsx
@@ -3,6 +3,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap";
 import { Filters } from "./components/Filters/Filters";
 import { Cards } from "./components/Cards/Cards";
+import { Pagination } from "./components/Pagination/Pagination";
 
 export const App = () => {
     let [pageNumber, setPageNumber] = useState(1);
@@ -32,11 +33,13 @@ export const App = () => {
                     <div className="col-8">
                         <div className="row">
                             <Cards results={results} />
-                            
+
                         </div>
                     </div>
                 </div>
             </div>
+
+            <Pagination pageNumber={pageNumber} setPageNumber={setPageNumber} />
 
 
         </div>

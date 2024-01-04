3be248ef0f6ee7900bb5ad759c31fafd42aae92d
tiago1820 <tiago.zdo@gmail.com>
2024-01-04 06:06:36 -0300

    Instagram Clone - Home Page

--- instagram/src/components/Sidebar/Sidebar.jsx
+++ instagram/src/components/Sidebar/Sidebar.jsx
@@ -3,6 +3,7 @@ import { Link as RouterLink } from "react-router-dom";
 import { InstagramLogo, InstagramMobileLogo, SearchLogo, NotificationsLogo, CreatePostLogo } from "../../assets/constants";
 
 import { AiFillHome } from "react-icons/ai";
+import { BiLogOut } from "react-icons/bi";
 
 export const Sidebar = () => {
     const sidebarItens = [
@@ -82,7 +83,7 @@ export const Sidebar = () => {
                                 borderRadius={6}
                                 p={2}
                                 w={{ base: 10, md: "full" }}
-                                justifyContent={{base:"center",md:"flex-start"}}
+                                justifyContent={{ base: "center", md: "flex-start" }}
 
                             >
                                 {item.icon}
@@ -92,10 +93,36 @@ export const Sidebar = () => {
                             </Link>
 
                         </Tooltip>
-                    ))
-
-                    }
+                    ))}
                 </Flex>
+                <Tooltip
+                    hasArrow
+                    label={"Logout"}
+                    placement="right"
+                    ml={1}
+                    openDelay={500}
+                    display={{ base: "block", md: "none" }}
+                >
+                    <Link
+                        display={"flex"}
+                        to={"/auth"}
+                        as={RouterLink}
+                        alignItems={"center"}
+                        gap={4}
+                        _hover={{ bg: "whiteAlpha.400" }}
+                        borderRadius={6}
+                        p={2}
+                        w={{ base: 10, md: "full" }}
+                        mt={{ base: 10, md: "full" }}
+                        justifyContent={{ base: "center", md: "flex-start" }}
+
+                    >
+                        <BiLogOut size={25} />
+                        <Box display={{ base: "none", md: "block" }}>
+                            Logout
+                        </Box>
+                    </Link>
+                </Tooltip>
             </Flex>
         </Box>
     )

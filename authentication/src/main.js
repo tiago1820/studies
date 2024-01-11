6b5408294fd50731bd1c5f39d9b8e4e8e362e726
tiago1820 <tiago.zdo@gmail.com>
2024-01-11 19:11:35 -0300

    Authentication with email

---
+++ authentication/src/main.js
@@ -0,0 +1,16 @@
+import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { auth } from "./app/firebase.js";
+import { loginCheck } from "./app/loginCheck.js";
+
+import "./app/signupForm.js";
+import "./app/signinForm.js";
+import "./app/logout.js";
+
+onAuthStateChanged(auth, async (user) => {  
+    loginCheck(user);
+    // if(user) {
+    //     loginCheck(user);
+    // } else {
+    //     loginCheck(user);
+    // }
+})

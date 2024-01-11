2688fe69f92f92e01b9ba94b10f389a0a5410338
tiago1820 <tiago.zdo@gmail.com>
2024-01-11 20:36:13 -0300

    Authentication with Google

--- authentication/src/app/firebase.js
+++ authentication/src/app/firebase.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-auth.js"
 
 // Your web app's Firebase configuration
 const firebaseConfig = {

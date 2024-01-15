c14d8e51b02cd22d97397466f92fe51923bbe271
tiago1820 <tiago.zdo@gmail.com>
2024-01-15 18:45:54 -0300

    Download File

--- nodejs-s3/index.js
+++ nodejs-s3/index.js
@@ -1,6 +1,6 @@
 import express from "express";
 import fileUpload from "express-fileupload";
-import { uploadFile, getFiles, getFile } from "./s3.js";
+import { uploadFile, getFiles, getFile, downloadFile } from "./s3.js";
 
 const app = express();
 
@@ -19,10 +19,17 @@ app.get("/files/:fileName", async (req, res) => {
     res.json(result);
 });
 
+app.get("/downloadfile/:fileName", async (req, res) => {
+    await downloadFile(req.params.fileName);
+    res.json({message: "archivo descargado"});
+});
+
 app.post("/files", async (req, res) => {
     const result = await uploadFile(req.files.file)
     res.json({ result })
 });
+
+app.use(express.static("images"));
 
 app.listen(3000)
 console.log(`Server on port ${3000}`);

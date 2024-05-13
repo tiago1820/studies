bf954be1338ffb69b19710873c83e36ec10b69ae
tiago <tiago.zdo@gmail.com>
2024-05-13 09:53:46 -0300

    feat: news mag project

---
+++ news-mag/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import { Navbar } from "./components/Navbar";
+import { NewsBoard } from "./components/NewsBoard";
+
+export const App = () => {
+  const [category, setCategory] = useState("general");
+  console.log(category);
+  return (
+    <div>
+      <Navbar setCategory={setCategory} />
+      <NewsBoard category={category} />
+    </div>
+  )
+}
